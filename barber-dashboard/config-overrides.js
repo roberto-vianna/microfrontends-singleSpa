eb669899c01a67bf69e2079a541c7af6d3650394
roberto-vianna <jrviana2021@gmail.com>
2026-03-05 01:14:49 -0300

    Fix: Corrige publicPath de produção

--- barber-dashboard/config-overrides.js
+++ barber-dashboard/config-overrides.js
@@ -3,7 +3,7 @@ module.exports = {
   webpack: (config) => {
     config.entry = path.resolve(__dirname, "src/single-spa-entry.js");
     config.output.libraryTarget = 'system';
-    config.output.publicPath = 'http://localhost:8083/'; 
+    config.output.publicPath = 'https://barber-dashboard-mfe.netlify.app/'; 
     return config;
   }
 };
