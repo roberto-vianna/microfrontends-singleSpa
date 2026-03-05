9758138d12a7246ba1e1af696174e68afa4e0b9f
roberto-vianna <jrviana2021@gmail.com>
2026-03-05 01:59:43 -0300

    Corrigido build do barber-dashboard

--- barber-dashboard/config-overrides.js
+++ barber-dashboard/config-overrides.js
@@ -4,13 +4,13 @@ module.exports = {
   webpack: (config) => {
     config.entry = path.resolve(__dirname, "src/single-spa-entry.js");
     config.output.libraryTarget = "system";
+    config.output.filename = "main.js"; 
     config.output.publicPath = "https://barber-dashboard-mfe.netlify.app/";
     config.optimization = {
-      splitChunks: false,
-      runtimeChunk: false,
-    };
+        splitChunks: false,
+        runtimeChunk: false,
+      };
     config.externals = ["react", "react-dom"];
-
     return config;
   },
 };
