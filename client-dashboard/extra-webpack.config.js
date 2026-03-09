604e30011aaf15f352df90b90d47523b7ce4d6a3
roberto-vianna <jrviana2021@gmail.com>
2026-03-08 22:52:32 -0300

    fix: define raiz do domínio /cliente

--- client-dashboard/extra-webpack.config.js
+++ client-dashboard/extra-webpack.config.js
@@ -1,8 +1,20 @@
-const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
+// const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
+
+// module.exports = (config, options) => {
+//   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
+
+//   // Feel free to modify this webpack config however you'd like to
+//   return singleSpaWebpackConfig;
+// };
+
+const singleSpaAngularWebpack = require("single-spa-angular/lib/webpack").default;
 
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
-  // Feel free to modify this webpack config however you'd like to
+  singleSpaWebpackConfig.output.libraryTarget = "system";
+  singleSpaWebpackConfig.output.chunkLoadingGlobal = "wpClientDashboard";
+  singleSpaWebpackConfig.output.publicPath = "/cliente/";
+
   return singleSpaWebpackConfig;
 };
