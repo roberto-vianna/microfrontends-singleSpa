922fd842af0bd9d5e22b2acb8a893e54f8be8522
roberto-vianna <jrviana2021@gmail.com>
2026-03-10 11:54:03 -0300

    fix: restaura entry main.js do barber-dashboard para import map

--- barber-dashboard/config-overrides.js
+++ barber-dashboard/config-overrides.js
@@ -4,6 +4,7 @@ module.exports = {
   webpack: (config) => {
     config.entry = path.resolve(__dirname, "src/single-spa-entry.js");
     config.output.libraryTarget = "system";
+    config.output.filename = "main.js";
     config.output.publicPath = "/barbeiro/";
     return config;
   },
