0c1cf143e183b710ea5559f2ff7acf07b2f1fe02
roberto-vianna <jrviana2021@gmail.com>
2026-03-11 10:03:03 -0300

    fix: carrega o estilo css dentro do single spa

--- barber-dashboard/src/single-spa-entry.js
+++ barber-dashboard/src/single-spa-entry.js
@@ -1,41 +1,41 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import singleSpaReact from "single-spa-react";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-// import './index.css';
-
-// const lifecycles = singleSpaReact({
-//   React,
-//   ReactDOM: ReactDOM,
-//   rootComponent: (props) => (
-//     <BrowserRouter basename="/barbeiro"> {} <App {...props} /> </BrowserRouter>
-//   ),
-//   domElementGetter: () => document.getElementById("barber-dashboard"),
-// });
-
-// export const { bootstrap, mount, unmount } = lifecycles;
 import React from "react";
 import ReactDOM from "react-dom/client";
 import singleSpaReact from "single-spa-react";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import './index.css';
 
 const lifecycles = singleSpaReact({
   React,
-  ReactDOM,
-  rootComponent: () => (
-    <div style={{ background: "#09090b", color: "#fff", padding: "20px" }}>
-      TESTE MICROFRONTEND BARBEIRO
-    </div>
+  ReactDOM: ReactDOM,
+  rootComponent: (props) => (
+    <BrowserRouter basename="/barbeiro"> {} <App {...props} /> </BrowserRouter>
   ),
   domElementGetter: () => document.getElementById("barber-dashboard"),
-  errorBoundary(err, info, props) {
-    return React.createElement(
-      "div",
-      { style: { padding: "16px" } },
-      "Erro ao carregar o dashboard do barbeiro new."
-    );
-  },
 });
 
 export const { bootstrap, mount, unmount } = lifecycles;
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import singleSpaReact from "single-spa-react";
+// import "./index.css";
+
+// const lifecycles = singleSpaReact({
+//   React,
+//   ReactDOM,
+//   rootComponent: () => (
+//     <div style={{ background: "#09090b", color: "#fff", padding: "20px" }}>
+//       TESTE MICROFRONTEND BARBEIRO
+//     </div>
+//   ),
+//   domElementGetter: () => document.getElementById("barber-dashboard"),
+//   errorBoundary(err, info, props) {
+//     return React.createElement(
+//       "div",
+//       { style: { padding: "16px" } },
+//       "Erro ao carregar o dashboard do barbeiro new."
+//     );
+//   },
+// });
+
+// export const { bootstrap, mount, unmount } = lifecycles;
